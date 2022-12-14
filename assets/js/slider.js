8caa26af351c869b372c48a8723321486b49b1a4
SabeehHasnain <sabeehhusnain@gmail.com>
2022-12-14 12:44:41 +0500

    First Push

---
+++ assets/js/slider.js
@@ -0,0 +1,47 @@
+// Logo Caoursal JavaScript
+$(document).ready(function () {
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
+// Img-Carousal JavaScript
+$(document).ready(function () {
+    $('.img-carousal').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
+});
---
+++ contact.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/webcom/assets/images/cropped-webcom08-180x180.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+    <link rel="stylesheet" href="/webcom/assets/css/style.css">
+    <title>Contact | Création Site Web Sur Mesure</title>
+</head>
+
+<body>
+    <!-- Services Banner Section -->
+    <section class="contact-banner-section">
+        <header class="page-header">
+            <nav class="navbar navbar-expand-lg" aria-label="Offcanvas navbar large">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#"><img src="/webcom/assets/images/webcom08-1.png" alt="logo"></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2" aria-controls="offcanvasNavbar2">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
+                        <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="offcanvasNavbar2Label"><img src="/webcom/assets/images/webcom08-1.png" alt="logo" width="100" height="100"></h5>
+                            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="https://devhubtest.website/webcom">Accueil</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="https://devhubtest.website/webcom/a-propos">A propos</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="https://devhubtest.website/webcom/demarche-web">Démarche
+                                        web</a>
+                                </li>
+                                <li class="nav-item dropdown desktop-dropdown-menu">
+                                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="https://devhubtest.website/webcom/services">Services</a>
+                                    <ul class="dropdown-menu column-3">
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <ul>
+                                                    <li><a href="https://devhubtest.website/webcom/services">SEO</a></li>
+                                                    <li><a href="https://devhubtest.website/webcom/services">Analyse des données</a></li>
+                                                    <li><a href="https://devhubtest.website/webcom/services">Architecture du site</a></li>
+                                                    <li><a href="https://devhubtest.website/webcom/services">Hébergement</a></li>
+                                                </ul>
+                                            </div>
+                                            <div class="col-sm-4">
+                                                <ul>
+                                                    <li><a href="https://devhubtest.website/webcom/services">Optimisation du contenu</a></li>
+                                                    <li><a href="https://devhubtest.website/webcom/services">Objectifs et cibles</a></li>
+                                                    <li><a href="https://devhubtest.website/webcom/services">Liens internes et externes</a></li>
+                                                    <li><a href="https://devhubtest.website/webcom/services">Sécurisation</a></li>
+                                                </ul>
+                                            </div>
+                                            <div class="col-sm-4">
+                                                <ul>
+                                                    <li><a href="https://devhubtest.website/webcom/services">Optimisation sur la page</a></li>
+                                                    <li><a href="https://devhubtest.website/webcom/services">Charte graphique</a></li>
+                                                    <li><a href="https://devhubtest.website/webcom/services">Maintenance</a></li>
+                                                    <li><a href="https://devhubtest.website/webcom/services">Tous
+                                                            les services</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </ul>
+                                </li>
+                                <li class="nav-item mobile-menu-item">
+                                    <a class="nav-link" href="https://devhubtest.website/webcom/services">Services</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="https://devhubtest.website/webcom/modeles-de-site">Modèles
+                                        de
+                                        site</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="https://devhubtest.website/webcom/contact">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <div class="contact-banner-content">
+            <div class="container">
+                <div class="row pt-5 pb-5 text-center">
+                    <h1 class="contact-main-title">Contact</h1>
+                    <h3 class="contact-sub-title"><a href="https://devhubtest.website/webcom">Accueil _ </a>Contact</h3>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Services Page Banner Ends Here -->
+
+    <!-- Drop A Line Section -->
+    <section class="drop-a-line-section">
+        <div class="container">
+            <div class="drop-a-line">
+                <div class="row">
+
+                    <div class="col-md-6">
+                        <div class="jumbotron">
+                            <h1>Contactez-nous</h1>
+                            <h2>Votre adresse e-mail ne sera pas publiée. Les champs obligatoires sont marqués *</h2>
+                            <form action="" method="post">
+                                <div class="pt-5 pb-3">
+
+                                    <input type="text" class="form-control" id="fullname" placeholder="Votre Prénom" name="fullname">
+                                </div>
+                                <div class="pt-5 pb-5">
+
+                                    <input type="email" class="form-control" id="email" placeholder="Email" name="email">
+                                </div>
+                                <div class="pt-3 pb-3">
+
+                                    <textarea name="message" id="message" class="form-control" placeholder="Message" cols="30" rows="10"></textarea>
+                                </div>
+                                <div class="g-recaptcha" data-sitekey="6LclPRcjAAAAAJqyAUHlUpus2d_FCuXzB5VyEIwv"></div>
+                                <br/>
+                                <input type="submit" name="submit" class="btn" value="Envoyer">
+                                <!-- <button type="submit" class="btn" >Envoyer</button> -->
+                            </form>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="contact-form-text">
+                            <h2>Nos coordonnées</h2>
+                            <h1>Commençons à travailler ensemble.<br>Nous sommes à votre écoute!</h1>
+                            <p>Contactez-nous pour discuter des vos besoins.<br> N'hésitez pas à nous appeller ou à nous envoyer un messge.</p>
+                            <h3>France</h3>
+                            <h3><a href="tel:0982509491">09 82 50 94 91</a></h3>
+                            <h3><a href="mailto:contact@webcom08.com">contact@webcom08.com</a></h3>
+                            <h4>
+                                <a href="#" class="fa-brands fa-twitter icon"></a>
+                                <a href="#" class="fa-brands fa-facebook icon"></a>
+                                <a href="#" class="fa-brands fa-linkedin icon"></a>
+                                <a href="#" class="fa-brands fa-instagram icon"></a>
+                            </h4>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Drop Line Section Ends Here -->
+
+    <!-- Footer Section -->
+  <footer class="footer-section">
+    <div class="container">
+      <div class="webcom-footer">
+        <div class="row">
+          <div class="col-md-12 col-lg-2">
+            <img src="/webcom/assets/images/webcom08-1.png" width="100px" height="100px" />
+          </div>
+          <div class="col-md-6 col-lg-3 footer-column">
+            <h1>Suivez-nous</h1>
+            <ul>
+              <li>Facebook</li>
+            </ul>
+          </div>
+          <div class="col-md-6 col-lg-3 footer-column">
+            <h1>Contacts</h1>
+            <ul>
+              <li>France</li>
+              <li><a href="tel:0982509491">09 82 50 94 91</a></li>
+              <li><a href="mailto:contact@webcom08.com">contact@webcom08.com</a></li>
+            </ul>
+          </div>
+          <div class="col-md-12 col-lg-3 footer-column">
+            <h1>Liens</h1>
+            <div class="pages">
+              <ul>
+                <li><a href="https://devhubtest.website/webcom">Accueil</a></li>
+                <li><a href="https://devhubtest.website/webcom/a-propos">A Propos</a></li>
+                <li><a href="https://devhubtest.website/webcom/demarche-web">Demarche Web</a></li>
+                <li><a href="https://devhubtest.website/webcom/services">Services</a></li>
+                <li><a href="https://devhubtest.website/webcom/modeles-de-site">Modeles de site</a></li>
+                <li><a href="https://devhubtest.website/webcom/contact">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <section class="copyright-section">
+    <div class="container footer-container">
+      <div class="copyright-footer">
+        <div class="row">
+          <div class="col-sm-12 col-md-9 col-lg-9 footer-column pre-footer">
+            <p>© Tous les droits réservés webcom08.</p>
+          </div>
+          <div class="col-sm-12 col-md-3 col-lg-3 footer-column pre-footer">
+          <a href="https://devhubtest.website/webcom/mentions-legales">Mentions-legales</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Footer Section Ends Here -->
+
+    <!-- Website JavaScript Links -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script>
+        $('.dropdown').hover(function() {
+            $(this).find('.column-3').stop(true, true).delay(200).fadeIn(100);
+        }, function() {
+            $(this).find('.column-3').stop(true, true).delay(200).fadeOut(100);
+        });
+    </script>
+</body>
+
+</html>
+<?php 
+$server = "localhost";
+$username = "root";
+$password = "";
+$con = mysqli_connect($server, $username, $password);
+if(!$con)
+{
+    echo "Connection Failed";
+}
+else{
+    echo "Connection Successful";
+}
+if(isset($_POST['submit']))
+{
+    $name = $_POST['fullname'];
+     $email = $_POST['email'];
+    $message = $_POST['message'];
+   echo "Button Clicked";
+   
+    $sql = "INSERT INTO `webcom`.`contact`(`Name`,`Email`,`Message`) VALUES ('$name', '$email', '$message')";
+    if($con->query($sql))
+    {
+        
+        echo "Data Added";
+    }
+    else
+    {
+        echo "Data not Added";
+    }
+}
+?>
